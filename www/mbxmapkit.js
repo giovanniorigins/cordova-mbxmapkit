5f7f8a4f0e7e9567007cccbd0aa7168d655cd7d0
Angelo Lakra <angelo.lakra@gmail.com>
2014-06-23 16:46:49 -0600

    Fixing bad javascript prototype definition

--- www/mbxmapkit.js
+++ www/mbxmapkit.js
@@ -9,50 +9,50 @@ function MBXMapKit() {
 };
 
 MBXMapKit.prototype = {
-  this.create  = function() {
+  create: function() {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to create native map view."); },
          "CDVMBXMapKit", "create", []);
-  };
+  },
 
-  this.destroy = function() {
+  destroy: function() {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to destroy native map view."); },
          "CDVMBXMapKit", "destroy", []);
-  };
+  },
 
-  this.show    = function() {
+  show: function() {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to show native map view."); },
          "CDVMBXMapKit", "show", []);
-  };
+  },
 
-  this.hide    = function() {
+  hide: function() {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to hide native map view."); },
          "CDVMBXMapKit", "hide", []);
-  };
+  },
 
-  this.move    = function(movementType, x, y) {
+  move: function(movementType, x, y) {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to move native map view."); },
          "CDVMBXMapKit", "move", [movementType, x, y]);
-  };
+  },
 
-  this.resize  = function(width, height) {
+  resize: function(width, height) {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to resize native map view."); },
          "CDVMBXMapKit", "resize", [width, height]);
-  };
+  },
 
-  this.changeType = function(mapType) {
+  changeType: function(mapType) {
     exec(function(params) { return null; },
          function(error)  { console.error("Failed to change the type of native map view."); },
          "CDVMBXMapKit", "changeType", [mapType]);
-  };
+  },
 
-  this.addAnnotation = function() {};
-  this.removeAnnotation = function() {};
+  addAnnotation: function() {},
+  removeAnnotation: function() {}
 };
 
 
