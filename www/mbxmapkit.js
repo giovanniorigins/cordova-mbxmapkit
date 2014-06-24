f309faa4bd343e6018e72d948c0f6da42be78f92
Angelo Lakra <angelo.lakra@gmail.com>
2014-06-24 15:55:01 -0600

    Trying to require q via namespace

--- www/mbxmapkit.js
+++ www/mbxmapkit.js
@@ -1,6 +1,6 @@
 var argscheck = require('cordova/argscheck'),
     exec      = require('cordova/exec'),
-    q         = require('q');
+    q         = require('com.alakra.cordova.mbxmapkit.q');
 
 function MBXMapKit() {
   this.view = null;
